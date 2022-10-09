49c0f0616047540588c6f3195c62990ed0ddd879
ArmanKeropyan <115360818+ArmanKeropyan@users.noreply.github.com>
2022-10-09 12:21:27 +0300

    my first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const symbol = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+function getNumberConv() {
+    let number = document.getElementById('number-10').value;
+    let base = document.getElementById('number-result-base').value;
+    console.log(number, base);
+
+}
